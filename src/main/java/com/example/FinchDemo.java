4543b166c56c7df4c6b8181fe29d84b336caa264
sofieschiff22 <sschiff622@icloud.com>
2026-05-28 13:34:18 -0700

    Update FinchDemo.java

--- src/main/java/com/example/FinchDemo.java
+++ src/main/java/com/example/FinchDemo.java
@@ -4,7 +4,7 @@ import com.birdbrain.Finch;
 
 public class FinchDemo {
     public static void main(String[] args) {
-        Finch myFinch = new Finch();
+        Finch myFinch = new Finch(); 
 
         myFinch.playNote(60,0.5);
 
