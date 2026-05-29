a1c16b4778669c87adb3f472e83fd5fbb01dac3e
sofieschiff22 <sschiff622@icloud.com>
2026-05-29 13:39:18 -0700

    Create TesterFinch.java

---
+++ src/main/sof/TesterFinch.java
@@ -0,0 +1,11 @@
+package com.sof; 
+
+
+public class TesterFinch{
+    public static void main (String[] args){
+        FinalFinch bird = new FinalFinch("A");
+
+        bird.setMove("F", 50, 100);
+
+    }
+}
