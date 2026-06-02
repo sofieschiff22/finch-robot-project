eb6ed37c086d5253da0ccca26e1176f31d029d0d
sofieschiff22 <sschiff622@icloud.com>
2026-06-02 13:37:07 -0700

    Update FinalFinch.java

--- src/main/java/com/sof/FinalFinch.java
+++ src/main/java/com/sof/FinalFinch.java
@@ -3,8 +3,17 @@ package com.sof;
 import com.birdbrain.Finch;
 
 public class FinalFinch extends Finch{
-   public FinalFinch(String device){
-    super(device);
 
-}
+   public FinalFinch(String device){
+        super(device);
+    }
+
+    public void drawPentagon() {
+        for (int i = 0; i < 5; i++) {
+            setMove("F", 5, 50);
+            setTurn("L", 72, 50);
+        }
+    }
+
+    
 }
