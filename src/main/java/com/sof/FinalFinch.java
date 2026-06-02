9f26d81e1eca6524a61037ec5c8d439486beab92
sofieschiff22 <sschiff622@icloud.com>
2026-06-02 13:39:46 -0700

    Update FinalFinch.java

--- src/main/java/com/sof/FinalFinch.java
+++ src/main/java/com/sof/FinalFinch.java
@@ -15,5 +15,26 @@ public class FinalFinch extends Finch{
         }
     }
 
+    Scanner input = new Scanner(System.in);
     
+    // Starting text
+    System.out.println("Welcome to my NLP app!");
+    System.out.println("1. Option 1");
+    System.out.println("2. Option 2");
+    String userInput = input.nextLine(); // collect userInput
+    
+    // Logic for userInput
+    if (userInput.equals("1")) {
+      System.out.println("Option 1 selected");
+      // add code here for option 1
+      
+    } else if (userInput.equals("2")) {
+      System.out.println("Option 2 selected");
+      // add code here for option 2
+      
+    } else {
+      // Error Handling
+      System.out.println("\nSorry, I dont understand that."); 
+      System.out.println("Please restart, and only choose option 1 or 2"); 
+    }
 }
