7348035702698b269f0d9037423cf1f8948f46da
sofieschiff22 <sschiff622@icloud.com>
2026-06-03 14:33:27 -0700

    Update Robot.java

--- src/main/java/com/birdbrain/Robot.java
+++ src/main/java/com/birdbrain/Robot.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URI;
 
 /**
  * This is an abstract class that is inherited by Microbit.java, Hummingbird.java and Finch.java.
@@ -105,7 +106,8 @@ abstract class Robot {
         long requestStartTime = System.currentTimeMillis();
 	    String responseString = "Not Connected";
         try {
-            requestUrl = new URL(URLRequest);
+            // requestUrl = new URL(URLRequest);
+            requestUrl = URI.create(URLRequest).toURL();
             connection = (HttpURLConnection) requestUrl.openConnection();
             connection.setRequestMethod("GET");
             //connection.setDoOutput(true);
