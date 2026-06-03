d5d958a6098373ee79f6cbd720652e822ce93a26
sofieschiff22 <sschiff622@icloud.com>
2026-06-03 14:33:25 -0700

    Update FinalFinch.java

--- src/main/java/com/sof/FinalFinch.java
+++ src/main/java/com/sof/FinalFinch.java
@@ -1,5 +1,7 @@
 package com.sof;
 
+import java.util.Scanner;
+
 import com.birdbrain.Finch;
 
 public class FinalFinch extends Finch{
@@ -8,33 +10,60 @@ public class FinalFinch extends Finch{
         super(device);
     }
 
-    public void drawPentagon() {
-        for (int i = 0; i < 5; i++) {
-            setMove("F", 5, 50);
-            setTurn("L", 72, 50);
-        }
+    public void askInput() {
+      Scanner input = new Scanner(System.in);
+    
+      // Starting text
+      System.out.println("Welcome to my Robot Shape Drawer");
+      System.out.println("1. Pentagon");
+      System.out.println("2. Square");
+      System.out.println("3. Rectangle");
+      String userInput = input.nextLine(); // collect userInput
+      
+      // Logic for userInput
+      if (userInput.equals("1")) {
+        System.out.println("Pentagon");
+        drawPentagon();
+      } else if (userInput.equals("2")) {
+        System.out.println("Square");
+        drawSquare();
+      } else if (userInput.equals("3")) {
+        System.out.println("Rectangle");
+        drawRectangle();
+      }else {
+        // Error Handling
+        System.out.println("\nSorry, I dont understand that."); 
+        System.out.println("Please restart, and only choose square, pentagon, or rectangle");
+      }
+  }
+
+    
+    public void drawSquare() {
+      for (int i = 0; i < 4; i++) {
+          setMove("F", 5, 50);
+          setTurn("L", 90, 50);
+      }
     }
 
-    Scanner input = new Scanner(System.in);
-    
-    // Starting text
-    System.out.println("Welcome to my NLP app!");
-    System.out.println("1. Option 1");
-    System.out.println("2. Option 2");
-    String userInput = input.nextLine(); // collect userInput
-    
-    // Logic for userInput
-    if (userInput.equals("1")) {
-      System.out.println("Option 1 selected");
-      // add code here for option 1
-      
-    } else if (userInput.equals("2")) {
-      System.out.println("Option 2 selected");
-      // add code here for option 2
-      
-    } else {
-      // Error Handling
-      System.out.println("\nSorry, I dont understand that."); 
-      System.out.println("Please restart, and only choose option 1 or 2"); 
+    public void drawPentagon() {
+      for (int i = 0; i < 5; i++) {
+          setMove("F", 5, 50);
+          setTurn("L", 72, 50);
+      }
+    }
+
+  public void drawRectangle() {
+      for (int i = 0; i < 1; i++) {
+          setMove("F", 10, 50);
+          setTurn("L", 90, 50);
+          setMove("F", 5, 50);
+          setTurn("L", 90, 50);
+          setMove("F", 10, 50);
+          setTurn("L", 90, 50);
+          setMove("F", 5, 50);
+
+
+
+      }
     }
 }
